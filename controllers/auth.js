4c8635621f6e91b9477003aa61e6d2a23f50f6f3
kitakoj18 <kitakoj18@gmail.com>
2020-07-17 17:50:13 -0700

    deleted userID in login controller since it is already taken care of in loggedin middlewear and dont need to send to front end

--- controllers/auth.js
+++ controllers/auth.js
@@ -103,7 +103,6 @@ exports.login = (req, res, next) =>{
 
             res.status(200).json({
                 token: token, 
-                userId: signedInUser._id.toString(),
                 expiresIn: expirationTime
             });
         })
